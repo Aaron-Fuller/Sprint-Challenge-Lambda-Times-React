79cfcb0fc8abbf17ce1392560ab7b7af45e853d6
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-06-12 14:44:57 -0400

    MVP

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import PropTypes from 'prop-types';
 
 const Cards = props => {
+  console.log("props in Cards, ", props)
   return (
     <div className="cards-container">
-      {cardData.map(cardFromMap => (
-          <Card card={cardFromMap} key={cardFromMap.name} />
-        ))
-        }
+      { 
+        props.cards.map( (card, index) => < Card key={index} card={card} /> )
+      }
     </div>
   )
 }
+
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      headline: PropTypes.string,
+      tab: PropTypes.string,
+      img: PropTypes.string,
+      author: PropTypes.string
+    })
+  )
+};
 
 // Make sure you include prop types for all of your incoming props
 
